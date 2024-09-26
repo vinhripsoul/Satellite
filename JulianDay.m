3210ab0141bd72db1f4769b2db97b5517f419709
vinhripsoul <vinhripsoul@gmail.com>
2024-09-26 18:06:16 -0400

    Delete JulianDay.m

--- JulianDay.m
+++
@@ -1,3 +0,0 @@
-function [JD] = JulianDay(year,month,day,hour,minute,second)
-    % Calculation
-        JD = 367*year - floor((7*(year+floor((month+9)/12)))/4) + floor((275*month)/9) + day + 1721013.5 + ((((second/60)+minute)/60)+hour)/24;
